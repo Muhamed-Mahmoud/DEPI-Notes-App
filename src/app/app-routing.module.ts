14e77e0685588dfccde6bc2607d49047a7a297a1
Mai Gamal Salah <maigamal86316@gmail.com>
2025-04-16 23:16:01 +0100

    added guards , nav links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { RegisterComponent } from './auth/register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-const routes: Routes = [
-  // { path: 'register', component: RegisterComponent },
-  { path: 'notes', loadChildren: () => import('./notes/notes.module').then(m => m.NotesModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  // { path: '', redirectTo: '/auth', pathMatch: 'full' }, // Redirect to the auth module by default
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent } // Wildcard route for a 404 page
+import { AuthGuard } from './shared/auth.guard';
 
+const routes: Routes = [
+  { 
+    path: 'notes', 
+    loadChildren: () => import('./notes/notes.module').then(m => m.NotesModule),
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
+  },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/core/guards/core.guard.ts
+++ src/app/core/guards/core.guard.ts
@@ -4,7 +4,5 @@ import { CanActivateFn } from '@angular/router';
 export const coreGuard: CanActivateFn = (route, state) => {
   console.log('Core Guard');
 
-
-  
   return false;
 };
