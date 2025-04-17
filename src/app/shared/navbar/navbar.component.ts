0b11765bf1bf4d42548626ac3f0426f9cb8a1bcc
Muhamed-Mahmoud <muhamedmahmouddd@gmail.com>
2025-04-17 12:28:49 +0200

    changed  nav-bar style & logic

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,49 +1,50 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, HostListener, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  navBar: HTMLElement | null = null;
-  collapse: HTMLElement | null = null;
-
-
-
-  constructor(private router: Router) { }
-  ngOnInit(): void {
-    this.navBar = document.querySelector(".navlost");
-    this.collapse = document.querySelector(".collapse");
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    // Get the height of the viewport
-    const viewportHeight = window.innerHeight;
-
-    // Check if elements exist
-    if (!this.navBar) return;
-
-    // Check scroll position
-    if (window.scrollY >= viewportHeight) {
-      this.navBar.classList.add("fixed-top");
-    } else {
-      this.navBar.classList.remove("fixed-top");
-    }
-  }
-
-
-
-
-
+export class NavbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('navbar') navBarRef!: ElementRef;
 
   showLogoutPopup: boolean = false;
 
-  logout(){
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => this.checkScroll(), 100); // wait till DOM rendered
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.checkScroll(); // initial check
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    const nav = this.navBarRef?.nativeElement;
+    const viewportHeight = window.innerHeight;
+
+    if (nav) {
+      if (window.scrollY >= viewportHeight) {
+        nav.classList.add("fixed-top");
+      } else {
+        nav.classList.remove("fixed-top");
+      }
+    }
+  }
+
+  logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/auth/login']);
-
   }
 }
